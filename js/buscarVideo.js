2bf6f2f1b57182a359712d35df2f7339dfb9f4c8
RudiJr <rudi.juniior@gmail.com>
2023-06-12 11:31:46 -0300

    Created eventListener for button and some const in buscarVideos function

--- js/buscarVideo.js
+++ js/buscarVideo.js
@@ -1,5 +1,19 @@
 import { conectaApi } from "./conectaApi";
 
+/**
+ * This function searches for a video using data input and an API connection.
+ */
 async function buscarVideo(){
-    const busca = await conectaApi.buscaVideo();
+    evento.preventDefault();
+
+    const dadosDePesquisa = document.querySelector("[data-pesquisa]").value;
+    const busca = await conectaApi.buscaVideo(dadosDePesquisa);
+
+    console.log(busca);
 };
+
+/* This code is adding an event listener to the `botaoDePesquisa` element, which is a button that
+triggers a search for a video. The event listener is listening for a 'click' event on the button,
+and when the event is triggered, it calls the `buscarVideo` function with the `evento` parameter. */
+const botaoDePesquisa = document.querySelector("[data-botao-pesquisa]");
+botaoDePesquisa.addEventListener('click', evento => buscarVideo(evento));
