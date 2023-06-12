f47ec0a436ef4d966db8811d661e4bc13a0f148e
RudiJr <rudi.juniior@gmail.com>
2023-06-12 11:07:52 -0300

    Created buscaVideo function and exported the function

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -1,3 +1,8 @@
+/**
+ * This function fetches a list of videos from a local server and returns it as a JSON object.
+ * @returns The function `listaVideos()` is returning a Promise that resolves to the JSON data
+ * retrieved from the URL "http://localhost:3000/videos".
+ */
 async function listaVideos() {
   const conexao = await fetch("http://localhost:3000/videos");
   const conexaoConvertida = await conexao.json();
@@ -5,6 +10,19 @@ async function listaVideos() {
   return conexaoConvertida;
 }
 
+/**
+ * The function creates a new video by sending a POST request to a server with the provided title,
+ * description, URL, and image.
+ * @param titulo - The title of the video being created.
+ * @param descricao - The description of the video, which is being concatenated with the string "mil
+ * visualizações" (thousand views) before being sent to the server.
+ * @param url - The URL of the video being created.
+ * @param imagem - The "imagem" parameter is a string representing the URL of the image associated with
+ * the video being created.
+ * @returns a Promise that resolves to the JSON response from the HTTP POST request made to the
+ * "http://localhost:3000/videos" endpoint. The JSON response is an object that represents the newly
+ * created video, with properties such as "titulo", "descricao", "url", and "imagem".
+ */
 async function criaVideo(titulo, descricao, url, imagem){
   const conexao = await fetch("http://localhost:3000/videos", {
     method: "POST",
@@ -23,7 +41,21 @@ async function criaVideo(titulo, descricao, url, imagem){
   return conexaoConvertida;
 };
 
+/**
+ * This function fetches videos from a local server based on a search term and returns the results in
+ * JSON format.
+ * @returns The function `buscaVideo` is returning a Promise that resolves to the JSON data obtained
+ * from the specified URL (`http://localhost:3000/videos?q=`).
+ */
+async function buscaVideo(){
+  const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`);
+  const conexaoConvertida = conexao.json();
+
+  return conexaoConvertida;
+};
+
 export const conectaApi = {
   listaVideos,
-  criaVideo
+  criaVideo,
+  buscaVideo
 };
