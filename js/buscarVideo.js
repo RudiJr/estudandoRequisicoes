7c4f768f796475bbccabbb3a3d07da0b8c94150b
RudiJr <rudi.juniior@gmail.com>
2023-06-12 11:41:50 -0300

    Add while to remove firstChild when user searched something

--- js/buscarVideo.js
+++ js/buscarVideo.js
@@ -1,18 +1,33 @@
 import { conectaApi } from "./conectaApi.js";
 import constroiCard from "./mostrarVideos.js";
 
-
 /**
- * This function searches for a video using data input and constructs a card with the video's
- * information.
+ * This is an async function that searches for a video using data from a form input, connects to an
+ * API, and constructs a card with information about the video.
  */
 async function buscarVideo(){
+ /* `evento.preventDefault();` is preventing the default behavior of the event. In this case, it is
+ preventing the default behavior of a form submission, which would cause the page to reload. By
+ calling `preventDefault()`, the function is able to handle the form submission without reloading
+ the page. */
     evento.preventDefault();
 
     const dadosDePesquisa = document.querySelector("[data-pesquisa]").value;
     const busca = await conectaApi.buscaVideo(dadosDePesquisa);
 
     const lista = document.querySelector("[data-lista]");
+
+  /* This code is removing all child elements from the `lista` element. It does this by using a `while`
+  loop to repeatedly remove the first child element of `lista` until there are no more child
+  elements left. This is useful for clearing the list before adding new elements to it. */
+    while(lista.firstChild){
+        lista.removeChild(lista.firstChild);
+    };
+
+    /* This code is iterating over each element in the `busca` array and constructing a card for each
+    video using the `constroiCard` function. The constructed card is then appended as a child
+    element to the `lista` element. In other words, it is adding a card with information about each
+    video to a list on the webpage. */
     busca.forEach(elemento => lista.appendChild(constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)));
 };
 
